581ed38694a74b7badcbf9cf50478f582ac2c683
Theodoros Ploumis <me@theodorosploumis.com>
2016-04-14 02:20:10 +0300

    Better docker aliases examples

--- examples/shortcuts/docker-aliases.sh
+++ examples/shortcuts/docker-aliases.sh
@@ -1,26 +1,33 @@
 #!/usr/bin/env bash
 
-# Docker
-alias docker-list="docker ps -a"
+# Docker related aliases
+
+# Stop all running containers
 alias docker-stop="docker stop $(docker ps -a -q)"
-#alias docker-remove-all="docker ps -a | awk '{print $1}' | xargs --no-run-if-empty docker rm"
-alias docker-info="docker info"
-alias docker-run="docker run -d -t -p "
+
+alias dinfo="docker info"
+alias drun="docker run -d -t -p "
+
+# Debug host ip
 # Check this issue https://github.com/docker/docker/issues/8395#issuecomment-58063080
 alias hostip="ip route show 0.0.0.0/0 | grep -Eo 'via \S+' | awk '{ print \$2 }'"
-alias docker-viz="/usr/bin/dockviz"
+
+# List all running containers
 alias dlive="docker ps"
+
+# List all containers
 alias dall="docker ps -a"
 
 # Remove all unamed images
-alias docker-rm-unnamed='docker rmi $(docker images | grep "^<none>" | awk "{print $3}")'
-#alias docker-volumes="/usr/bin/docker-volumes"
+alias docker-rm-unnamed="docker rmi $(docker images | grep '^<none>' | awk '{print $3}')"
 
 # Remove all containers that have exited
-function docker-rm() {
+function docker-rm-exited() {
   docker rm -v $(docker ps -a -q -f status=exited)
 }
 
+# Remove all containers
+# ToDo: Add an interactive question to proceed
 function docker-rm-all() {
   echo -n "Stopping..."
   docker stop $(docker ps -a -q)
@@ -28,18 +35,19 @@ function docker-rm-all() {
   docker rm $(docker ps -a -q)
 }
 
-# Login into a docker container
+# Login into a docker container with bash
 # Usage: docker-ssh-bash [container]
 function docker-ssh-bash() {
-    docker exec -i -t "$@" /bin/bash
+   docker exec -i -t "$@" /bin/bash
 }
 
+# Login into a docker container with sh
 # Usage: docker-ssh-sh [container]
 function docker-ssh-sh() {
-    docker exec -i -t "$@" /bin/sh
+  docker exec -i -t "$@" /bin/sh
 }
 
-# Pull all images
+# Pull all images (this may take time...)
 function docker-pull-all(){
   docker images | awk '(NR>1) && ($2!~/none/) {print $1":"$2}' | xargs -L1 docker pull
 }
